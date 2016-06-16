de5717635324da7584fec92d6b64c1eed90372e4
Jim Balhoff <jim@balhoff.org>
2016-06-16 09:42:32 -0400

    Added data download script. Updated readme.

---
+++ 0_download_data.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+# These are taken from the Monarch scigraph-data config.
+# You need owltools installed for getting the imports of the ontologies.
+
+set -e
+
+mkdir data
+cd data
+mkdir ontologies
+
+curl -L -O http://data.monarchinitiative.org/owl/monarch-merged.owl
+owltools http://biohackathon.org/resource/faldo.ttl --slurp-import-closure -d ontologies -c catalog-v001.xml
+owltools http://purl.obolibrary.org/obo/eco.owl --slurp-import-closure -d ontologies -c catalog-v001.xml
+owltools http://purl.obolibrary.org/obo/iao.owl --slurp-import-closure -d ontologies -c catalog-v001.xml
+owltools https://raw.githubusercontent.com/monarch-initiative/GENO-ontology/develop/src/ontology/geno.owl --slurp-import-closure -d ontologies -c catalog-v001.xml
+owltools http://data.monarchinitiative.org/owl/ero.owl --slurp-import-closure -d ontologies -c catalog-v001.xml
+owltools http://purl.obolibrary.org/obo/pw.owl --slurp-import-closure -d ontologies -c catalog-v001.xml
+owltools https://raw.githubusercontent.com/jamesmalone/OBAN/master/ontology/oban_core.ttl --slurp-import-closure -d ontologies -c catalog-v001.xml
+## change to RDF/XML?
+  # clo.owl is missing and import
+  #- url: http://purl.obolibrary.org/obo/clo.owl
+owltools http://purl.obolibrary.org/obo/pco.owl --slurp-import-closure -d ontologies -c catalog-v001.xml
+owltools http://purl.obolibrary.org/obo/xco.owl --slurp-import-closure -d ontologies -c catalog-v001.xml
+owltools http://xmlns.com/foaf/spec/index.rdf --slurp-import-closure -d ontologies -c catalog-v001.xml
+owltools http://purl.org/dc/elements/1.1/ --slurp-import-closure -d ontologies -c catalog-v001.xml
+owltools http://xmlns.com/foaf/0.1/ --slurp-import-closure -d ontologies -c catalog-v001.xml
+
+curl -L -O http://data.monarchinitiative.org/ttl/mgi.ttl
+curl -L -O http://data.monarchinitiative.org/ttl/panther.ttl
+curl -L -O http://data.monarchinitiative.org/ttl/clinvar.ttl
+curl -L -O http://data.monarchinitiative.org/ttl/ncbigene.ttl
+curl -L -O http://data.monarchinitiative.org/ttl/biogrid.ttl
+curl -L -O http://data.monarchinitiative.org/ttl/zfin.ttl
+curl -L -O http://data.monarchinitiative.org/ttl/ctd.ttl
+curl -L -O http://data.monarchinitiative.org/ttl/hpoa.ttl
+curl -L -O http://data.monarchinitiative.org/ttl/omim.ttl
+curl -L -O http://data.monarchinitiative.org/ttl/coriell.ttl
+curl -L -O http://data.monarchinitiative.org/ttl/hgnc.ttl
+curl -L -O http://data.monarchinitiative.org/ttl/kegg.ttl
+curl -L -O http://data.monarchinitiative.org/ttl/impc.ttl
+curl -L -O http://data.monarchinitiative.org/ttl/ucscbands.ttl
+#curl -L -O http://data.monarchinitiative.org/ttl/cgd.ttl    # this is not ready yet
+curl -L -O http://data.monarchinitiative.org/ttl/monochrom.ttl
+curl -L -O http://data.monarchinitiative.org/ttl/eom.ttl
+curl -L -O http://data.monarchinitiative.org/ttl/genereviews.ttl
+curl -L -O http://data.monarchinitiative.org/ttl/orphanet.ttl
+curl -L -O http://data.monarchinitiative.org/ttl/animalqtldb.ttl
+curl -L -O http://data.monarchinitiative.org/ttl/flybase.nt
+curl -L -O http://data.monarchinitiative.org/ttl/wormbase.nt
+curl -L -O http://data.monarchinitiative.org/ttl/omia.ttl
+curl -L -O http://data.monarchinitiative.org/ttl/mmrrc.ttl
+curl -L -O http://data.monarchinitiative.org/ttl/gwascatalog.ttl
+curl -L -O http://data.monarchinitiative.org/ttl/monarch.ttl
+curl -L -O http://data.monarchinitiative.org/ttl/mpd.ttl
+curl -L -O http://data.monarchinitiative.org/ttl/go.ttl
+
+# Blazegraph will error if it tries to load this
+rm ontologies/catalog-v001.xml
--- README.md
+++ README.md
@@ -1,3 +1,28 @@
 ##Performance##
 
 Running `clique_merge.py` after loading Blazegraph with all Monarch data is taking just under 2 hours on a Macbook Pro 13" with SSD hard drive.
+
+###Top 20 clique sizes###
+
+|                      Leader                            | Members |
+| ------------------------------------------------------ | ------- |
+| <http://www.ncbi.nlm.nih.gov/clinvar/variation/187907> |   137   |
+| <http://identifiers.org/animalqtltrait/1049>           |   102   |
+| <http://identifiers.org/animalqtltrait/1053>           |   81    |
+| <http://www.ncbi.nlm.nih.gov/clinvar/variation/15091>  |   77    |
+| <http://jaxmice.jax.org/strain/000162>                 |   77    |
+| <http://identifiers.org/animalqtltrait/10>             |   53    |
+| <http://identifiers.org/FB:FBgn0003941>                |   46    |
+| <http://www.ncbi.nlm.nih.gov/pubmed/6690234>           |   42    |
+| <http://www.informatics.jax.org/accession/MGI:1935197> |   35    |
+| <http://identifiers.org/animalqtltrait/1009>           |   31    |
+| <http://identifiers.org/animalqtltrait/1181>           |   26    |
+| <http://www.ncbi.nlm.nih.gov/clinvar/variation/143949> |   26    |
+| <http://identifiers.org/animalqtltrait/1107>           |   25    |
+| <http://www.informatics.jax.org/accession/MGI:1298370> |   23    |
+| <http://www.ncbi.nlm.nih.gov/pubmed/12196193>          |   23    |
+| <http://identifiers.org/FB:FBgn0039545>                |   22    |
+| <http://identifiers.org/FB:FBgn0246409>                |   22    |
+| <http://identifiers.org/FB:FBgn0040034>                |   21    |
+| <http://identifiers.org/animalqtltrait/1085>           |   20    |
+| <http://identifiers.org/animalqtltrait/1223>           |   20    |
